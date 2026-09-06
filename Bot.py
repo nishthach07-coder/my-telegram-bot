98e15f421d81727c96bbbeed6ea89025172c09a0
nishthach07-coder <nishthach07@gmail.com>
2026-09-06 17:41:27 +0530

    Create Bot.py

---
+++ Bot.py
@@ -0,0 +1,12 @@
+import os
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Namaste! Main GitHub se live hoon! 🚀")
+
+if __name__ == '__main__':
+    token = os.getenv("TELEGRAM_TOKEN")
+    app = ApplicationBuilder().token(token).build()
+    app.add_handler(CommandHandler("start", start))
+    app.run_polling()
